c431a1e627e964e136f42b4a3622cb7908e53428
hayuntamientoc <gilberto.hdez79@gmail.com>
2025-07-01 00:04:36 -0600

    Se eleimina Sitio de prueba

--- WebAppExamen/WebAppExamen/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppExamen.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
